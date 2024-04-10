dae4fe95a7fdad057dff11e60d308226a5999185
Gabriel Mazieri <gabrieldias7200@gmail.com>
2024-04-10 09:01:50 -0300

    Declaração de uma model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'author',
+        'published_at',
+        'cover_image',
+    ];
 }
