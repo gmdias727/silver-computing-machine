7fe7fc2141b0307ebd5f48c14b5e483548ef9003
Gabriel Mazieri <gabrieldias7200@gmail.com>
2024-04-10 09:01:36 -0300

    Declaração de rotas

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/books', [BookController::class, 'index']);
+Route::post('/books', [BookController::class, 'store']);
+Route::put('/books/{id}', [BookController::class, 'update']);
+Route::delete('/books', [BookController::class, 'destroy']);
